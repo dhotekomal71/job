528733e755b08692c7784853d32d76b623102c4c
dhotekomal71 <dhotekomal71@gmail.com>
2024-10-08 14:33:55 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 class Simple{
     public static void main(String args[]){
-        System.out.println("Hello Students from Batch-36");
+        System.out.println("Hello Students from Batch-36 Komal yugal autl sakshi");
     }
 }
